04db25b7a8c7ede0e20b0654667ae0f5e660fe39
Mark Tilton <mark.tilton.a@gmail.com>
2019-11-10 22:12:33 -0500

    Create compensation grid

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ original_img = np.asarray(mpimg.imread('grid.png'))
 def warp_point(point):
     yn = point[0]
     xn = point[1]
-    xo = math.sin(yn*math.pi) * 0.1 # 10% horizontal warp
-    yo = 0# math.sin(xn*math.pi) * 0.1 # 10% horizontal warp
+    xo = math.sin(yn*math.pi*2) * 0.1 # 10% horizontal warp
+    yo = math.sin(xn*math.pi) * 0.1 # 10% vertical warp
     return (yn + yo, xn + xo)
 
 # Create warped image
@@ -29,8 +29,8 @@ for y in range(shape[0]):
             warped_img[y, x, c] = sample
 
 # Create warped point grid
-grid_h = 5
-grid_w = 5
+grid_h = 10
+grid_w = 10
 grid_x = []
 grid_y = []
 grid = np.zeros((grid_w, grid_h, 2))
@@ -43,29 +43,33 @@ for yi in range(grid_h):
         grid_x.append((point[1] * shape[1]))
         grid[xi][yi] = (point[1] * shape[1], point[0] * shape[0])
 
-# imgplot = plt.imshow(warped_img, zorder=1)
-# plt.scatter(grid_x, grid_y, zorder=2)
-# plt.show()
-# quit()
+# Create a compensation function
+compensation_grid = np.zeros((grid_w, grid_h, 2))
+for yi in range(grid_h):
+    for xi in range(grid_w):
+        x = xi / (grid_w - 1) * shape[1]
+        y = yi / (grid_h - 1) * shape[0]
+        grid_point = grid[xi][yi]
+        compensation_grid[xi][yi] = (2 * x - grid_point[0], 2 * y - grid_point[1])
 
 # Unwarp the image
 unwarped_img = np.zeros(shape)
 for y in range(shape[0]):
-    yn = y / shape[0]
+    yn = y / shape[0]     
     for x in range(shape[1]):
         xn = x / shape[1]
-        yi = yn * (grid.shape[0] - 1)
-        xi = xn * (grid.shape[1] - 1)
+        yi = yn * (compensation_grid.shape[0] - 1)
+        xi = xn * (compensation_grid.shape[1] - 1)
         yi0 = math.floor(yi)
         yi1 = yi0 + 1
         xi0 = math.floor(xi)
         xi1 = xi0 + 1
         ynn = yi - yi0
         xnn = xi - xi0
-        p00 = vmath.Vector2(grid[xi0][yi0])
-        p01 = vmath.Vector2(grid[xi0][yi1])
-        p10 = vmath.Vector2(grid[xi1][yi0])
-        p11 = vmath.Vector2(grid[xi1][yi1])
+        p00 = vmath.Vector2(compensation_grid[xi0][yi0])
+        p01 = vmath.Vector2(compensation_grid[xi0][yi1])
+        p10 = vmath.Vector2(compensation_grid[xi1][yi0])
+        p11 = vmath.Vector2(compensation_grid[xi1][yi1])
         t = (p10 - p00)
         b = (p11 - p01)
         pt = t.length * xnn * t.normalize() + p00
@@ -75,8 +79,9 @@ for y in range(shape[0]):
         nx = int(p[0])
         ny = int(p[1])
         for c in range(shape[2]):
-            sample = warped_img[ny, nx, c] if nx >= 0 and ny >= 0 and nx < shape[1] and ny < shape[0] else 0
-            unwarped_img[y, x, c] = sample
+            sample = warped_img[y, x, c]
+            if nx >= 0 and ny >= 0 and nx < shape[1] and ny < shape[0]:
+                unwarped_img[ny, nx, c] = sample
 
 # Show plots
 imgplot = plt.imshow(unwarped_img, zorder=1)
